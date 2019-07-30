22607006794c37b7e528d81da2b623b81fe9437b
paultrott <7577205+paultrott@users.noreply.github.com>
2019-07-30 19:10:50 +0800

    React version
    React Test

---
+++ precache-manifest.c67a062e1f4406b0ab797e06054cb01b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2f80543ff2b05acbf677deac64ed1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8063e6f53034ecfb3091",
+    "url": "/static/css/main.8268f5f0.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "8063e6f53034ecfb3091",
+    "url": "/static/js/main.6c293ec9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
